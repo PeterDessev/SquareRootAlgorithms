927303e5f46d8cee2a45ae963946f56e7688f0d8
Peter Dessev <peter.dessev@gmail.com>
2021-08-21 16:39:39 -0600

    first conecept for how to organize testing

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 #include "include/roots.h"
 
 int main(){
+    // Array of functions for producing a guess
+    int (*producing[]) (float input) = {
+        floatingPoint,
+        inverseSquare
+    };
 
+    // Array of functions for iterating a guess     
+    int (*iterating[]) (float input, float estimate) = {
+        goldschmidt,
+        lucas,
+        newton,
+        halley
+    };
+
+    float result = (*producing[0]) (1.0f);
     return 0;
 }
