0a46a523f68db3cf719b52a335d77c0b1f1d3337
Peter Dessev <peter.dessev@gmail.com>
2021-08-21 22:11:01 -0600

    Comments and chronology

--- include/roots.h
+++ include/roots.h
@@ -1,9 +1,11 @@
-#include "IteratingAnEstimate/goldschmidt.h"
-#include "IteratingAnEstimate/lucas.h"
-#include "IteratingAnEstimate/newton.h"
-#include "IteratingAnEstimate/halley.h"
-
+// Producing algorithms
 #include "ProducingAnEstimate/floatingpoint.h"
 #include "ProducingAnEstimate/inverseSquare.h"
 #include "ProducingAnEstimate/inputOver.h"
 #include "ProducingAnEstimate/oneAsAnEstimate.h"
+
+// Iterating algorithms
+#include "IteratingAnEstimate/goldschmidt.h"
+#include "IteratingAnEstimate/lucas.h"
+#include "IteratingAnEstimate/newton.h"
+#include "IteratingAnEstimate/halley.h"
