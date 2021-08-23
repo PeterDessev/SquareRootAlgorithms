42fa211064b6617405211fc39b1cbcefb72a3f7b
Peter Dessev <peter.dessev@gmail.com>
2021-08-23 00:11:24 -0600

    added not implemented message

--- src/IteratingAnEstimate/halley.c
+++ src/IteratingAnEstimate/halley.c
@@ -1,5 +1,11 @@
 #include "IteratingAnEstimate/halley.h"
 
-float halley(float input){
-    
+float halley(float input, float estimate){
+    printf("HALLEY NOT YET IMPLEMENTED\n");
+    return 0.0f;
+}
+
+float halleyInverse(float input, float estimate){
+    printf("HALLEY INVERSE NOT YET IMPLEMENTED\n");
+    return 0.0f;
 }
