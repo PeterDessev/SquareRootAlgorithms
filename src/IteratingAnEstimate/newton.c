1ee8c55e706f16b25a77d0718d632bb0d3952dec
Peter Dessev <peter.dessev@gmail.com>
2021-08-23 00:11:09 -0600

    implementing more algorithms

--- src/IteratingAnEstimate/newton.c
+++ src/IteratingAnEstimate/newton.c
@@ -2,11 +2,25 @@
 
 #include "IteratingAnEstimate/newton.h"
 
-float newton(float input){
-    float S = 100;
-    float E = 1;
-    for(int i = 0 ; i < 10; i++){
-        E = 0.5f * E + (S / (2 * E));
+float newton(float input, float estimate){
+    float check = 0;
+
+    while(check < 0.9999 || check > 1.0001){
+        estimate = 0.5f * estimate + (input / (2 * estimate));
+        check = (estimate * estimate) * input;
     }
-    printf("%f\n", E);
+
+    return estimate;
+}
+
+float newtonInverse(float input, float estimate){
+    float inputOver2 = 0.5f * input;
+    float check = 0;
+
+    while(check < 0.9999 || check > 1.0001){
+        estimate = estimate * (1.5f - inputOver2 * estimate * estimate);
+        check = (estimate * estimate) * input;
+    }
+
+    return estimate;
 }
--- src/ProducingAnEstimate/inverseSquare.c
+++ src/ProducingAnEstimate/inverseSquare.c
@@ -1,5 +1,11 @@
+#include <stdint.h>
+
 #include "ProducingAnEstimate/inverseSquare.h"
 
+static union { float f; uint32_t i; } val;
+
 float inverseSquare(float input){
-    
+    val.f = input;
+    val.i = 0x5f375a86 - (val.i >> 1);
+    return val.f;
 }
