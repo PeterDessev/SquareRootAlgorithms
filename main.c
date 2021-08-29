24589a9b69531bbda9a86356397bab7da48df48d
Peter Dessev <peter.dessev@gmail.com>
2021-08-28 18:39:05 -0600

    Reworked testing methodology for more consistency

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
+// Detect if is being compiled on unix
+#if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__)))
+#define IS_UNIX
+#endif
+
+#ifdef IS_UNIX
+#include <sys/time.h>
+#endif
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 #include "include/roots.h"
 
-#define DEBUG
+#define DEBUG 0
 
 int main(){
     time_t beginBenchmark;
@@ -11,24 +20,25 @@ int main(){
 
     double seconds;
 
-    // This variable controls how many times the program will run the algorithm
-    // for each permutation of producers, iterators, and inputs
-    static const int benchmarkCount = 1000;
+    // This variable controls how mnay seconds the 
+    // program will give each permutation of algorithms
+    // to compute as many square roots as possible
+    static const double benchmarkTime = 0.1;
 
     // These two arrays are used to easily convert 
     // between the index for the address of a function
     // and its actual name for printing purposes
     char* prodNames[] = {
-        "inputOver",
-        "oneAsAnEstimate",
-        "floatingPoint",
-        "inverseSquare"
+        "Input Over S",
+        "One As An Estimate",
+        "Floating Point",
+        "Reciprocal Floating Point"
     };
 
     char* iterNames[] = {
         "Goldschmidt",
-        "newton",
-        "halley"
+        "Newton",
+        "Halley"
     };
 
     // Array of functions for producing a guess
@@ -51,40 +61,82 @@ int main(){
         halleyInverse
     };
 
+    // This array stores the results of the benchmark
+    long int results[prodCount][iterCount][21];
+
 
     // Begin testing
     // First loop to iterate through producing an estimate
     for(int prodIndex = 0; prodIndex < prodCount; prodIndex++){
         // Second loop to iterate through iterating on an estimate
         for(int iterIndex = 0; iterIndex < iterCount; iterIndex++){
+            // Helper for storing results, more reliable than log10(input)
+            int inputIndex = 0; 
+
+            // Because the inverse square root method produces an estimate for the inverse square root,
+            // a different set of functions is required to itareate over. They are contained in the
+            // iterating function pointer array, but with an offset of 4 from their non-inverted counterparts
+            int trueIterIndex = prodIndex == 3 ? iterIndex + 3 : iterIndex;
+            
+            float(* producer)(float) = producing[prodIndex];
+            float(* iterator)(float input, float estimate) = iterating[trueIterIndex];
+
+#if DEBUG >= 2
+            printf("%s with %s:\n", prodNames[prodIndex], iterNames[iterIndex]);
+#endif // DEBUG >= 2
             // Third loop dictates what number to find the square root of
             for(float input = 1E-10; input < 1E10; input *= 10){
-#ifdef DEBUG
-                printf("Testing %s with %s, calculating the square root of %f 1000 times...\n", prodNames[prodIndex], iterNames[iterIndex], input);
-#endif
-                // Because the inverse square root method produces an estimate for the inverse square root,
-                // a different set of functions is required to itareate over. They are contained in the
-                // iterating function pointer array, but with an offset of 4 from their non-inverted counterparts
-                int trueIterIndex = prodIndex == 3 ? iterIndex + 4 : iterIndex;
-                float(* producer)(float) = producing[prodIndex];
-                float(* iterator)(float input, float estimate) = iterating[trueIterIndex];
+#if DEBUG >= 3
+                printf("Testing %s with %s, calculating the square root of %f for %f seconds...\n", prodNames[prodIndex], iterNames[iterIndex], input, benchmarkTime);
+#endif // DEBUG >= 3
+                long int count = 0;
 
+                // Begin benchmark
+#ifdef IS_UNIX 
+                struct timeval tval_before, tval_after, tval_result;
+                double timeSpent;
+
+                gettimeofday(&tval_before, NULL);
+                gettimeofday(&tval_after, NULL);
+
+                timersub(&tval_after, &tval_before, &tval_result);
+
+                timeSpent = (double)(long)tval_result.tv_sec + 
+                    ((double)(long)tval_result.tv_usec )/ (double)1000000;
+
+                while(timeSpent < benchmarkTime){
+                    gettimeofday(&tval_after, NULL);
+                    timersub(&tval_after, &tval_before, &tval_result);
+                    
+                    timeSpent = (double)(long)tval_result.tv_sec + 
+                        ((double)(long)tval_result.tv_usec )/ (double)1000000;
+
+#else // ifdef IS_UNIX 
                 // Start time
                 time(&beginBenchmark);
 
-                for(int count = 0; count < benchmarkCount; count++){
+                while(difftime(time(NULL), beginBenchmark) < benchmarkTime){
+#endif // ifdef IS_UNIX 
                     float estimate = (*producer) (input);
                     float result = (*iterator) (input, estimate);
+                    count++;
                 }
-                
-                // End time
-                time(&endBenchmark);
-                seconds = difftime(beginBenchmark, endBenchmark);
+                results[prodIndex][iterIndex][inputIndex++] = count;
 
-                printf("It took %lf seconds for %s with %s to finish calculating the square root of %f 1000 times\n\n", seconds, prodNames[prodIndex], iterNames[iterIndex], input);
+#if DEBUG >= 1
+#ifdef IS_UNIX
+                printf("%lf\n", timeSpent);
+#else
+                double bmTime = difftime(time(NULL), beginBenchmark);
+                printf("%lf\n", bmTime);
+#endif // ifdef IS_UNIX
+#endif // if DEBUG >= 1
+
+#if DEBUG >= 2
+                printf("%c with %c on %.1e: %ld\n", prodNames[prodIndex][0], iterNames[iterIndex][0], input, count);
+#endif // if DEBUG >= 2
             }
         }
     }
-
     return 0;
 }
