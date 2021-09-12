d7ac890d0b4ced05d6882e400f48c42ec6d08590
Peter Dessev <peter.dessev@gmail.com>
2021-09-11 22:30:22 -0600

    Fixed and slightly improved input over

--- src/ProducingAnEstimate/inputOver.c
+++ src/ProducingAnEstimate/inputOver.c
@@ -1,5 +1,12 @@
 #include "ProducingAnEstimate/inputOver.h"
 
 float inputOver(float input){
-    return (input >= 1 ? input * 2.0f : input * 0.5f);
+    if(input > 1)
+        return input / 2;
+    else if(input < 1) 
+        return input * 2;
+    // input must equal 1, adding this does not slow the other two down, but makes 
+    // claculating the square root of 1 faster.
+    else 
+        return 1;
 }
