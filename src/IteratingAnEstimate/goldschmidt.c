b8d188f3ea84cc726b454e29c855c4fbaeaa95d2
Peter Dessev <peter.dessev@gmail.com>
2021-09-24 08:24:18 -0600

    added check to inverse and refactor

--- src/IteratingAnEstimate/goldschmidt.c
+++ src/IteratingAnEstimate/goldschmidt.c
@@ -19,25 +19,25 @@ float goldschmidt(float input, float estimate){
         check = (G * G) / input;
         count++;
     }
-    if(count == 99 || isnan(G) || isinf(G))
-        return -1;
-    
-    return G;
+
+    return count == 99 || isnan(G) || isinf(G) ? -1 : G;
 }
 
 float goldschmidtInverse(float input, float estimate){
     double R, G, H, check;
+    int count = 0;
 
+    //estimate = 1 / estimate;
     G = input * estimate;
     H = estimate / 2;
 
-    while(check < 0.9999f || check > 1.0001f){
+    while((check < 0.9999f || check > 1.0001f) && count < 100){
         R = 0.5f - G * H;
         G = G + G * R;
         H = H + H * R;
-
-        check = (4.0f * H * H) * input;
+        check = (G * G) / input;
+        count++;
     }
 
-    return 2 * H;
+    return count == 99 || isnan(G) || isinf(G) ? -1 : 2 * H;
 }
